af6318f93ca32961a14a81346718021a4165d2e4
Vee <ifebuchejulia@gmail.com>
2024-02-23 14:22:10 +0100

    concluded the roadmap section

--- src/components/section/Roadmap.jsx
+++ src/components/section/Roadmap.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import styled from "styled-components";
 import DrawSvg from "../DrawSvg";
+import { useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Section = styled.section`
   min-height: 100vh;
@@ -34,10 +37,9 @@ const Container = styled.div`
 
 const SvgContainer = styled.div`
   display: flex;
-  justify-content: center; 
+  justify-content: center;
   align-items: center;
 `;
-
 
 const Items = styled.ul`
 list-style:none;
@@ -47,68 +49,125 @@ display:flex;
 flex-direction:column;
 justify-content:center;
 align-items:center;
-background-color:lightblue;
-`
 
-const Item = styled.li`
-width:100%;
-height:100%;
-display:flex;
+&>*:nth-of-type(2n+1){
+  justify-content:start;
 
-`
-const ItemContainer = styled.div`
-width:40%;
-height:fit-content;
-padding:1rem;
-border:3px solid ${props=>props.theme.text};
-`
+  div{
+    border-radius: 50px 0 50px 0;
+    text-align:right;
+  }
+  p{
+    border-radius: 40px 0 40px 0;
 
-const SubTitle = styled.span`
-display:block;
-font-size:${props=> props.theme.fontxl};
-text-transform: capitialize;
-color:${props=>props.theme.text};
-`
+  }
+}
+&>*:nth-of-type(2n){
+  justify-content:end;
+  div{
+    border-radius:0 50px 0 50px;
+    text-align-left;
+  }
+  p{
+    border-radius:0 40px 0 40px;
 
-const Text = styled.span`
-display:block;
-font-size:${props=> props.theme.fontsm};
-text-transform: capitialize;
-color:${props=>props.theme.text};
-
-font-weight:400;
-margin:0.5rem 0;
-
-`
-
-const Box= styled.p`
-height:fit-content;
-background-color:${props=> props.theme.carouselColor};
-color:${props=> props.theme.text};
-padding:1rem;
-position:relative;
-border:1px solid ${props=> props.theme.text};
-`
-
-const RoadMapItem = ({title, subText}) =>{
-  return(
-    <Item>
-      <ItemContainer>
-<Box>
-  <SubTitle>
-{title}
-  </SubTitle>
-  <Text>
-    {subText}
-  </Text>
-</Box>
-      </ItemContainer>
-    </Item>
-  )
+  }
 }
 
 
+`;
+
+const Item = styled.li`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+const ItemContainer = styled.div`
+  width: 40%;
+  height: fit-content;
+  padding: 1rem;
+  border: 3px solid ${(props) => props.theme.text};
+`;
+
+const SubTitle = styled.span`
+  display: block;
+  font-size: ${(props) => props.theme.fontxl};
+  text-transform: capitialize;
+  color: ${(props) => props.theme.text};
+`;
+
+const Text = styled.span`
+  display: block;
+  font-size: ${(props) => props.theme.fontsm};
+  text-transform: capitialize;
+  color: ${(props) => props.theme.text};
+
+  font-weight: 400;
+  margin: 0.5rem 0;
+`;
+
+const Box = styled.p`
+  height: fit-content;
+  background-color: ${(props) => props.theme.carouselColor};
+  color: ${(props) => props.theme.text};
+  padding: 1rem;
+  position: relative;
+  border: 1px solid ${(props) => props.theme.text};
+`;
+
+const RoadMapItem = ({ title, subText, addToRefs }) => {
+  return (
+    <Item ref={addToRefs}>
+      <ItemContainer>
+        <Box>
+          <SubTitle>{title}</SubTitle>
+          <Text>{subText}</Text>
+        </Box>
+      </ItemContainer>
+    </Item>
+  );
+};
+
 const Roadmap = () => {
+  const revealRefs = useRef([]);
+  revealRefs.current = [];
+  gsap.registerPlugin(ScrollTrigger);
+  const addToRefs = (el) => {
+    if (el && !revealRefs.current.includes(el)) {
+      revealRefs.current.push(el);
+    }
+  };
+
+  useLayoutEffect(() => {
+    let t1 = gsap.timeline();
+   
+revealRefs.current.forEach((el, index)=>{
+
+  t1.fromTo(
+    el.childNodes[0],
+    {
+      y: "0"
+    },{
+      y:"-30%",
+
+      scrollTrigger:{
+        id: `section-${index+1}`,
+        trigger: el,
+        start: "top center+=200px",
+        end: "bottom center",
+        scrub:true,
+       
+      }
+    }
+  )
+
+} )
+
+
+
+
+    return () => {};
+  }, []);
   return (
     <Section>
       <Title>RoadMap</Title>
@@ -117,11 +176,32 @@ const Roadmap = () => {
           <DrawSvg />
         </SvgContainer>
         <Items>
-         <RoadMapItem title="This is title" subText="This is Sub Text"/>
-         <RoadMapItem title="This is title" subText="This is Sub Text"/>
-         <RoadMapItem title="This is title" subText="This is Sub Text"/>
-         <RoadMapItem title="This is title" subText="This is Sub Text"/>
-         <RoadMapItem title="This is title" subText="This is Sub Text"/>
+          <Item>&nbsp;</Item>
+          <RoadMapItem
+            addToRefs={addToRefs}
+            title="Grand Opening"
+            subText="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
+          <RoadMapItem
+            addToRefs={addToRefs}
+            title="Great Benefits"
+            subText="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
+          <RoadMapItem
+            addToRefs={addToRefs}
+            title="Early Access"
+            subText="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
+          <RoadMapItem
+            addToRefs={addToRefs}
+            title="New Merch"
+            subText="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
+          <RoadMapItem
+            addToRefs={addToRefs}
+            title="Holders Ranking"
+            subText="Lorem Ipsum Dolor Sit Amet Consectetur, Adipisicing Elit. At Repellat Placeat, Adipisicing Elit. At Repellat Placeat."
+          />
         </Items>
       </Container>
     </Section>
