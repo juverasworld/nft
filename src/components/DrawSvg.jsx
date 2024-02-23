80f5b5b43672da5730b4b9bfa09879fd8a19966a
Vee <ifebuchejulia@gmail.com>
2024-02-23 09:12:09 +0100

    started the roadMap although i've done the svg animation but we're still building you know

---
+++ src/components/DrawSvg.jsx
@@ -0,0 +1,96 @@
+import React, { useLayoutEffect, useRef } from "react";
+import Vector from "../Icons/Vector";
+import styled, { keyframes } from "styled-components";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const VectorContainer = styled.div`
+  position: absolute;
+  top: 0.5rem;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Bounce = keyframes`
+from{ transform:translateX(-50%) scale(0.5);}
+to{ transform:translateX(-50%) scale(1);}
+
+`;
+
+const Ball = styled.div`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.text};
+  animation: ${Bounce} 0.5s linear infinite alternate;
+`;
+
+const DrawSvg = () => {
+  const ref = useRef(null);
+  const ballref = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
+  useLayoutEffect(() => {
+    let element = ref.current;
+
+    let svg = document.getElementsByClassName("svg-path")[0];
+    const length = svg.getTotalLength();
+
+    // start postioning of svg drawing
+    svg.style.strokeDasharray = length;
+
+    // Hide svg before scrolling start
+    svg.style.strokeDashoffset = length;
+
+    let t1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: element,
+        start: "top center",
+        end: "bottom bottom",
+        onUpdate: (self) => {
+          const draw = length * self.progress;
+
+          // also reverse the drawing when scroll goes up
+
+          svg.style.strokeDashoffset = length - draw;
+        },
+        
+        onToggle: (self) => {
+          if (self.isActive) {
+            ballref.current.style.display = "none";
+          } else {
+            ballref.current.style.display = "inline-block";
+          }
+        },
+      },
+    });
+    return () => {
+
+        if(t1) t1.kill();
+    };
+  }, []);
+
+  return (
+    <>
+      <Ball ref={ballref} />
+
+      <VectorContainer ref={ref}>
+        <Vector />
+      </VectorContainer>
+    </>
+  );
+};
+
+export default DrawSvg;
--- src/components/section/Roadmap.jsx
+++ src/components/section/Roadmap.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import DrawSvg from "../DrawSvg";
 
 const Section = styled.section`
   min-height: 100vh;
@@ -15,18 +16,114 @@ const Title = styled.h1`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin:1rem auto;
+  margin: 1rem auto;
   border-bottom: 2px solid ${(props) => props.theme.text};
-  width:fit-content;
-
-
-  
+  width: fit-content;
 `;
+
+const Container = styled.div`
+  width: 70%;
+  height: 200vh;
+  background-color: ${(props) => props.theme.body};
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const SvgContainer = styled.div`
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+`;
+
+
+const Items = styled.ul`
+list-style:none;
+width:100%;
+height:100%;
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+background-color:lightblue;
+`
+
+const Item = styled.li`
+width:100%;
+height:100%;
+display:flex;
+
+`
+const ItemContainer = styled.div`
+width:40%;
+height:fit-content;
+padding:1rem;
+border:3px solid ${props=>props.theme.text};
+`
+
+const SubTitle = styled.span`
+display:block;
+font-size:${props=> props.theme.fontxl};
+text-transform: capitialize;
+color:${props=>props.theme.text};
+`
+
+const Text = styled.span`
+display:block;
+font-size:${props=> props.theme.fontsm};
+text-transform: capitialize;
+color:${props=>props.theme.text};
+
+font-weight:400;
+margin:0.5rem 0;
+
+`
+
+const Box= styled.p`
+height:fit-content;
+background-color:${props=> props.theme.carouselColor};
+color:${props=> props.theme.text};
+padding:1rem;
+position:relative;
+border:1px solid ${props=> props.theme.text};
+`
+
+const RoadMapItem = ({title, subText}) =>{
+  return(
+    <Item>
+      <ItemContainer>
+<Box>
+  <SubTitle>
+{title}
+  </SubTitle>
+  <Text>
+    {subText}
+  </Text>
+</Box>
+      </ItemContainer>
+    </Item>
+  )
+}
+
 
 const Roadmap = () => {
   return (
     <Section>
       <Title>RoadMap</Title>
+      <Container>
+        <SvgContainer>
+          <DrawSvg />
+        </SvgContainer>
+        <Items>
+         <RoadMapItem title="This is title" subText="This is Sub Text"/>
+         <RoadMapItem title="This is title" subText="This is Sub Text"/>
+         <RoadMapItem title="This is title" subText="This is Sub Text"/>
+         <RoadMapItem title="This is title" subText="This is Sub Text"/>
+         <RoadMapItem title="This is title" subText="This is Sub Text"/>
+        </Items>
+      </Container>
     </Section>
   );
 };
