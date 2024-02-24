3c9e769b0f168a4099be3333ad4bd6e67ea5e744
Vee <ifebuchejulia@gmail.com>
2024-02-24 08:17:13 +0100

    started the FAQ section

--- src/components/section/Faq.jsx
+++ src/components/section/Faq.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100%;
+  background-color: ${(props) => props.theme.text};
+ color: ${(props) => props.theme.body};
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: uppercase;
+  color: ${(props) => props.theme.body};
+  margin: 1rem auto;
+  border-bottom: 2px solid ${(props) => props.theme.carouselColor};
+  width: fit-content;
+`;
+
+const Container = styled.div`
+  width: 75%;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  width: 45%;
+`;
 const Faq = () => {
   return (
-    <div>Faq</div>
-  )
-}
+    <Section>
+      <Title>Faq</Title>
+      <Container>
+        <Box>col 1</Box>
+        <Box>col 1</Box>
+      </Container>
+    </Section>
+  );
+};
 
-export default Faq
+export default Faq;
