ad5d35a1f6c047f7082c15cf17b7b91070af4d85
Vee <ifebuchejulia@gmail.com>
2024-02-24 08:58:29 +0100

    created the accordion

--- src/components/section/Faq.jsx
+++ src/components/section/Faq.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import styled from "styled-components";
+import Accordion from "../Accordion";
+import { useRef } from "react";
 
 const Section = styled.section`
   min-height: 100vh;
   width: 100%;
   background-color: ${(props) => props.theme.text};
- color: ${(props) => props.theme.body};
+  color: ${(props) => props.theme.body};
   position: relative;
   display: flex;
   justify-content: center;
@@ -34,12 +36,61 @@ const Box = styled.div`
   width: 45%;
 `;
 const Faq = () => {
+
+const ref = useRef(null);
+
+useLayoutEffect(() => {
+  
+
+  return () => {
+    
+  };
+}, [])
+
   return (
-    <Section>
+    <Section ref={ref}>
       <Title>Faq</Title>
       <Container>
-        <Box>col 1</Box>
-        <Box>col 1</Box>
+        <Box>
+          <Accordion title="WHERE CAN I VIEW MY NFTS?">
+            Once minted or bought simply connect to your OpenSea account to view
+            your NFTs.
+          </Accordion>
+          <Accordion title="WHAT IS THE METAVERSE?">
+            A metaverse is a network of 3D virtual worlds focused on social
+            connection. In futurism and science fiction, it is often described
+            as a hypothetical iteration of the Internet as a single, universal
+            virtual world that is facilitated by the use of virtual and
+            augmented reality headsets.
+          </Accordion>
+          <Accordion title="WHY DO WE NEED ROYALTIES?">
+            The amount of royalties was fixed at 5% to finance the Weirdos
+            Club's projects. We have the ambition to organize multiple events
+            around the world in order to strengthen the community and build a
+            network of entrepreneurs and investors with the same mindset and
+            common interests.
+          </Accordion>
+        </Box>
+        <Box>
+          <Accordion title="WHY DO WE NEED ROYALTIES?">
+            The amount of royalties was fixed at 5% to finance the Weirdos
+            Club's projects. We have the ambition to organize multiple events
+            around the world in order to strengthen the community and build a
+            network of entrepreneurs and investors with the same mindset and
+            common interests.
+          </Accordion>
+          <Accordion title="HOW CAN I USE MY NFT?">
+            You will be able to use your NFT as an avatar in the Metaverse and
+            our future video game. Holding also means that you are part of an
+            exclusive network of investors and entrepreneurs.
+          </Accordion>
+          <Accordion title=" WHAT ARE THE WEIRDOS?">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            deserunt consequatur quisquam maxime molestias dolores ipsum,
+            exercitationem vel sint quidem aliquam modi quis impedit corporis
+            unde inventore fugiat provident in.
+          </Accordion>
+        </Box>
       </Container>
     </Section>
   );
