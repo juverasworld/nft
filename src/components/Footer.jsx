511ee071c4445f13a6c8dee0e00513f4b81df875
Vee <ifebuchejulia@gmail.com>
2024-02-25 05:20:55 +0100

    completed the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import Banner from "./Banner";
 import Logo from "./Logo";
+import Twitter from "../Icons/Twitter";
+
+import Instagram from "../Icons/Instagram";
 import Facebook from "../Icons/Facebook";
+import LinkedIn from "../Icons/LinkedIn";
 
 const Section = styled.section`
   min-height: 100vh;
@@ -36,7 +40,7 @@ const Left = styled.div`
 const IconList = styled.div`
   display: flex;
   align-items: center;
-  margin: 0 auto;
+  margin: 1rem auto;
 
   & > * {
     padding-right: 0.5rem;
@@ -48,6 +52,43 @@ const IconList = styled.div`
   }
 `;
 
+const MenuItems = styled.ul`
+  list-style: none;
+  width: 50%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-gap: 1rem;
+`;
+const Item = styled.li`
+  width: fit-content;
+  cursor: pointer;
+
+  &::after {
+    content: " ";
+    display: block;
+    width: 0%;
+    height: 2px;
+    background: ${(props) => props.theme.text};
+    transition: width 0.3s ease;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+`;
+
+const Bottom = styled.div`
+  width: 75%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a{
+    text-decoration:underline;
+  }
+`;
 const Footer = () => {
   return (
     <Section>
@@ -56,24 +97,49 @@ const Footer = () => {
         <Left>
           <Logo />
           <IconList>
-            <a href="https://facebook.com" target={"_blank"} rel="noopener">
-              <Facebook />
+            <a href="https://www.linkedin.com/obasi-vera-a8b155198" target={"_blank"} rel="noopener">
+              <LinkedIn/>
+            </a>
+            <a href="https://twitter.com/Vera_Ifebuche" target={"_blank"} rel="noopener">
+              <Twitter />
             </a>
             <a href="https://facebook.com" target={"_blank"} rel="noopener">
               <Facebook />
             </a>
-            <a href="https://facebook.com" target={"_blank"} rel="noopener">
-              <Facebook />
-            </a>
-            <a href="https://facebook.com" target={"_blank"} rel="noopener">
-              <Facebook />
+            <a href="https://Instagram.com" target={"_blank"} rel="noopener">
+              <Instagram />
             </a>
             <a href="https://facebook.com" target={"_blank"} rel="noopener">
               <Facebook />
             </a>
           </IconList>
         </Left>
+        <MenuItems>
+          <Item>Home</Item>
+          <Item>About</Item>
+          <Item>RoadMap</Item>
+          <Item>Showcase</Item>
+          <Item>Team</Item>
+          <Item>Faq</Item>
+        </MenuItems>
       </Container>
+      <Bottom>
+        <span>
+          $copy; {new Date().getFullYear()} Weirdos CLub. All right reserved.
+        </span>
+        <span>
+          Made with &#10084; by
+          <a
+            href=" https://vera-black.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Vee
+          </a>
+        
+        </span>
+      </Bottom>
     </Section>
   );
 };
